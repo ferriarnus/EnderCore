39efab99973ee107ab4a8908ce48ec5de63f07ba
ferriarnus <ferri.arnus@gmail.com>
2021-09-23 13:29:00 +0200

    ported ScrollableListWidget
    ported ScrollableListWidget, but render calls will need to be double checked.

--- src/main/java/com/enderio/core/api/client/gui/ListSelectionListener.java
+++ src/main/java/com/enderio/core/api/client/gui/ListSelectionListener.java
@@ -1,11 +1,11 @@
-//package com.enderio.core.api.client.gui;
-//
-//import com.enderio.core.client.gui.widget.ScrollableListWidget;
-//
-//import javax.annotation.Nonnull;
-//
-//public interface ListSelectionListener<T> {
-//
-//  void selectionChanged(@Nonnull ScrollableListWidget<T> list, int selectedIndex);
-//
-//}
+package com.enderio.core.api.client.gui;
+
+import com.enderio.core.client.gui.widget.ScrollableListWidget;
+
+import javax.annotation.Nonnull;
+
+public interface ListSelectionListener<T> {
+
+  void selectionChanged(@Nonnull ScrollableListWidget<T> list, int selectedIndex);
+
+}
